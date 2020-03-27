450a7feeb6b856237be4511b67501864a670da08
Eve Stockstill <evestockstillpdx@gmail.com>
2020-03-27 11:12:35 -0700

    switch branch/checking travis

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.scss';
-import mp4 from '../assets/glitter3.mov';
+import mov from '../assets/glitter3.mov';
 
 const Header = () => {
   return (
@@ -9,7 +9,7 @@ const Header = () => {
         <header className='header'>
           <div className='bg-video'>
             <video className='bg-video__content' autoPlay muted loop>
-              <source src={mp4} type='video/mp4' alt='video' />
+              <source src={mov} type='video/mp4' alt='video' />
               your browser not supported
             </video>
           </div>
